4d30f85e73b1d7fa50e538d94fa6b1044066603f
Jonathan Wilkins <wilkinsphysics@gmail.com>
2017-01-26 22:08:30 +0000

    fixed adduser

--- bin/adduser.sh
+++ bin/adduser.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Script to add a user to Linux system
-if [ $(id -u) -eq 0 ]; then
+if [ "$(id -u)" -eq 0 ]; then
     read -p "enter username : " username
     read -s -p "enter password : " password
     egrep "^$username" /etc/passwd >/dev/null
@@ -8,8 +8,8 @@ if [ $(id -u) -eq 0 ]; then
         echo -e "\n$username already exists"
         exit 1
     else
-        pass=$(perl -e 'print crypt($ARGV[0], "password")' $password)
-        useradd -m -p $pass $username
+        pass=$(perl -e 'print crypt($ARGV[0], "password")' "$password")
+        useradd -m -p "$pass" "$username"
         [ $? -eq 0 ] && echo -e "\nUser successfully added" || echo -e "\nFailed to add user"
     fi
 else
