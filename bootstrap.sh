22dda85ef8ddffb712f64639ec4290d70d7bc71f
AUTHORNAME <AUTHOREMAIL>
2017-03-25 14:00:07 +0000

    fix printf statements in bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,25 +6,25 @@ echo ''
 
 # util functions
 info () {
-  printf "\r  [ \033[00;34m..\033[0m ] $1\n"
+  printf "\r  [ \033[00;34m..\033[0m ] %s\n" "$1"
 }
 
 user () {
-  printf "\r  [ \033[0;33m??\033[0m ] $1\n"
+  printf "\r  [ \033[0;33m??\033[0m ] %s\n" "$1"
 }
 
 success () {
-  printf "\r\033[2K  [ \033[00;32mOK\033[0m ] $1\n"
+  printf "\r\033[2K  [ \033[00;32mOK\033[0m ] %s\n" "$1"
 }
 
 fail () {
-  printf "\r\033[2K  [\033[0;31mFAIL\033[0m] $1\n"
+  printf "\r\033[2K  [\033[0;31mFAIL\033[0m] %s\n" "$1"
   echo ''
   exit
 }
 
 setup_gitconfig () {
-  if ! [ -f $HOME/.gitconfig ]
+  if ! [ -f "$HOME/.gitconfig" ]
   then
     info 'setup gitconfig'
 
@@ -139,7 +139,7 @@ setup_gitconfig
 install_dotfiles
 
 # if we're on a mac, let's install and setup homebrew
-if [ "$(findcurrentos)" == "Darwin" ]
+if [ "$(uname -s)" == "Darwin" ]
 then
   info 'installing dependencies'
   if source macos/osx-init > /tmp/osx-install 2>&1
