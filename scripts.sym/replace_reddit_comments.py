0625e32384a61f49f31edf16f1517a00f44ef416
W1lkins <wilkins@linux.com>
2018-04-21 15:59:51 +0100

    add dmenu scripts, remove unnecessary scripts

--- scripts.sym/replace_reddit_comments.py
+++
@@ -1,5 +0,0 @@
-import praw
-r = praw.Reddit(user_agent="Mass comment editing script")
-r.login("username", "password")
-for a in r.user.get_comments(limit=1000):
-    a.edit("Replacement text here!")
---
+++ scripts.sym/rpi_christmas.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""Control lights on a raspberry pi"""
+
+from signal import pause
+from gpiozero import LEDBoard
+from gpiozero.tools import random_values
+
+TREE = LEDBoard(*range(2, 28), pwm=True)
+
+for led in TREE:
+    led.source_delay = 1
+    led.source = random_values()
+pause()
