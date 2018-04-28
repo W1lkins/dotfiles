9b91ac83e1226dea7cd58c74148de666746f54a3
W1lkins <wilkins@linux.com>
2018-04-28 19:18:04 +0000

    gofmt

--- main.go
+++ main.go
@@ -1,25 +1,25 @@
 package main
 
 import (
-    "bytes"
+	"bytes"
 	"flag"
-    "fmt"
-    "io"
-    "io/ioutil"
-    "os"
-    "os/exec"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"os"
+	"os/exec"
 	"os/signal"
-    "runtime"
-    "strings"
+	"runtime"
+	"strings"
 	"syscall"
 
+	"github.com/mitchellh/go-homedir"
 	"github.com/sirupsen/logrus"
-    "github.com/mitchellh/go-homedir"
-    input "github.com/tcnksm/go-input"
+	input "github.com/tcnksm/go-input"
 )
 
 const (
-    BANNER = `
+	BANNER = `
 blaat
 
 Set up dotfiles for the current system
@@ -28,8 +28,8 @@ Build: %s
 )
 
 var (
-    debug bool
-    vrsn bool
+	debug bool
+	vrsn  bool
 )
 
 var GITCOMMIT string
@@ -68,102 +68,102 @@ func main() {
 		}
 	}()
 
-    setupGitConfig()
+	setupGitConfig()
 }
 
 func setupGitConfig() {
-    home, err := homedir.Dir()
-    if err != nil {
-        logrus.Fatalf("Could not get home directory %s", err)
-    }
+	home, err := homedir.Dir()
+	if err != nil {
+		logrus.Fatalf("Could not get home directory %s", err)
+	}
 
-    gcfg := home + "/.gitconfig"
-    if (!fileExists(gcfg)) {
-        logrus.Info("Setting up gitconfig")
-        store := "cache"
-        if runtime.GOOS == "darwin"{
-            store = "osxkeychain"
-        }
+	gcfg := home + "/.gitconfig"
+	if !fileExists(gcfg) {
+		logrus.Info("Setting up gitconfig")
+		store := "cache"
+		if runtime.GOOS == "darwin" {
+			store = "osxkeychain"
+		}
 
-        name := askUser("What is your Github author name?", &input.Options{
-            Default: "W1lkins",
-            Required: true,
-            Loop: true,
-        })
+		name := askUser("What is your Github author name?", &input.Options{
+			Default:  "W1lkins",
+			Required: true,
+			Loop:     true,
+		})
 
-        email := askUser("What is your Github author email?", &input.Options{
-            Default: "wilkins@linux.com",
-            Required: true,
-            Loop: true,
-        })
+		email := askUser("What is your Github author email?", &input.Options{
+			Default:  "wilkins@linux.com",
+			Required: true,
+			Loop:     true,
+		})
 
-        useGPG := askUser("Do you want to use a GPG key with git? [y/N]", &input.Options{
-            Default: "N",
-            Required: true,
-            Loop: true,
-        })
+		useGPG := askUser("Do you want to use a GPG key with git? [y/N]", &input.Options{
+			Default:  "N",
+			Required: true,
+			Loop:     true,
+		})
 
-        usingGPG := strings.HasPrefix(strings.ToLower(useGPG), "y")
+		usingGPG := strings.HasPrefix(strings.ToLower(useGPG), "y")
 
-        var key string
-        if (usingGPG) {
-            keys, err := runCommand("gpg", "--list-secret-keys", "--keyid-format", "LONG")
-            if err != nil {
-                logrus.Fatalf("Could not get GPG keys: %s", err)
-            }
-            logrus.Info(keys.String())
+		var key string
+		if usingGPG {
+			keys, err := runCommand("gpg", "--list-secret-keys", "--keyid-format", "LONG")
+			if err != nil {
+				logrus.Fatalf("Could not get GPG keys: %s", err)
+			}
+			logrus.Info(keys.String())
 
-            key = askUser("Which key?", &input.Options{
-                Required: true,
-                Loop: true,
-            })
-        }
+			key = askUser("Which key?", &input.Options{
+				Required: true,
+				Loop:     true,
+			})
+		}
 
-        localFile := "./git/gitconfig"
-        if (!fileExists("./git/gitconfig")) {
-            logrus.Fatal("Could not find ./git/gitconfig")
-        }
+		localFile := "./git/gitconfig"
+		if !fileExists("./git/gitconfig") {
+			logrus.Fatal("Could not find ./git/gitconfig")
+		}
 
-        err := copyFile(localFile, gcfg)
-        if err != nil {
-            logrus.Fatalf("Could not copy %s to %s: %s", localFile, gcfg, err)
-        }
+		err := copyFile(localFile, gcfg)
+		if err != nil {
+			logrus.Fatalf("Could not copy %s to %s: %s", localFile, gcfg, err)
+		}
 
-        // replace placeholder values
-        read, err := ioutil.ReadFile(gcfg)
-        new := strings.Replace(string(read), "AUTHORNAME", name, -1)
-        new = strings.Replace(new, "AUTHOREMAIL", email, -1)
-        new = strings.Replace(new, "GIT_CREDENTIAL_HELPER", store, -1)
-        if (usingGPG) {
-            new = strings.Replace(new, "AUTHORGPGKEY", key, -1)
-            new = strings.Replace(new, "gpgsign = false", "gpgsign = true", -1)
-        }
+		// replace placeholder values
+		read, err := ioutil.ReadFile(gcfg)
+		new := strings.Replace(string(read), "AUTHORNAME", name, -1)
+		new = strings.Replace(new, "AUTHOREMAIL", email, -1)
+		new = strings.Replace(new, "GIT_CREDENTIAL_HELPER", store, -1)
+		if usingGPG {
+			new = strings.Replace(new, "AUTHORGPGKEY", key, -1)
+			new = strings.Replace(new, "gpgsign = false", "gpgsign = true", -1)
+		}
 
-        err = ioutil.WriteFile(gcfg, []byte(new), 0)
-        if err != nil {
-            logrus.Fatalf("Could not replace contents of file %s: %s", gcfg, err)
-        }
+		err = ioutil.WriteFile(gcfg, []byte(new), 0)
+		if err != nil {
+			logrus.Fatalf("Could not replace contents of file %s: %s", gcfg, err)
+		}
 
-        logrus.Info("gitconfig created")
-    } else {
-        logrus.Info("skipped gitconfig")
-    }
+		logrus.Info("gitconfig created")
+	} else {
+		logrus.Info("skipped gitconfig")
+	}
 }
 
 func fileExists(f string) bool {
-    _, err := os.Stat(f)
-    return !os.IsNotExist(err)
+	_, err := os.Stat(f)
+	return !os.IsNotExist(err)
 }
 
 func askUser(query string, opt *input.Options) string {
-    ui := input.DefaultUI()
+	ui := input.DefaultUI()
 
-    o, err := ui.Ask(query, opt)
-    if err != nil {
-        logrus.Fatalf("Error while asking %s, %s", query, err)
-    }
+	o, err := ui.Ask(query, opt)
+	if err != nil {
+		logrus.Fatalf("Error while asking %s, %s", query, err)
+	}
 
-    return o
+	return o
 }
 
 func runCommand(command string, args ...string) (bytes.Buffer, error) {
@@ -171,10 +171,10 @@ func runCommand(command string, args ...string) (bytes.Buffer, error) {
 	cmd.Stdin = os.Stdin
 	cmd.Stderr = os.Stderr
 
-    var out bytes.Buffer
+	var out bytes.Buffer
 	cmd.Stdout = &out
 
-    err := cmd.Run()
+	err := cmd.Run()
 	if err != nil {
 		return out, err
 	}
@@ -183,41 +183,22 @@ func runCommand(command string, args ...string) (bytes.Buffer, error) {
 }
 
 func copyFile(src, dst string) error {
-    from, err := os.Open(src)
-    if err != nil {
-        return err
-    }
-    defer from.Close()
+	from, err := os.Open(src)
+	if err != nil {
+		return err
+	}
+	defer from.Close()
 
-    to, err := os.OpenFile(dst, os.O_RDWR|os.O_CREATE, 0664)
-    if err != nil {
-        return err
-    }
-    defer to.Close()
+	to, err := os.OpenFile(dst, os.O_RDWR|os.O_CREATE, 0664)
+	if err != nil {
+		return err
+	}
+	defer to.Close()
 
-    _, err = io.Copy(to, from)
-    if err != nil {
-        return err
-    }
+	_, err = io.Copy(to, from)
+	if err != nil {
+		return err
+	}
 
-    return nil
+	return nil
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
