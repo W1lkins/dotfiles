a879f250dc3d7a0f750e0dee7c588a46dd1c8c0e
evalexpr <jonathan@wilkins.tech>
2020-02-15 15:39:54 +0000

    install: ensure works for both mac and linux

--- install.sh
+++ install.sh
@@ -1,9 +1,7 @@
 #!/bin/bash -e
 # shellcheck disable=SC2044
 set -o pipefail
-
 DOTFILES_ROOT=$(pwd -P)
-export PATH="$PATH:/usr/local/go/bin:$HOME/.cargo/bin:$HOME"/.local/bin
 
 info() {
     printf "[\\033[00;34m.\\033[0m] %s\\n" "$1"
@@ -19,7 +17,12 @@ warn() {
 
 fail() {
     printf "\\033[2K[\\033[0;31m✘\\033[0m] %s\\n" "$1"
-    exit
+    exit 1
+}
+
+finish() {
+    success "$1"
+    exit 0
 }
 
 user_input() {
@@ -103,11 +106,7 @@ link_file() {
 }
 
 setup_sudo() {
-    sudo groupadd sudo || true
-    sudo groupadd docker || true
-    sudo groupadd systemd-journal || true
-    sudo groupadd systemd-network || true
-
+    info "setting up sudo"
     sudo gpasswd -a "$USER" sudo || true
     sudo gpasswd -a "$USER" systemd-journal || true
     sudo gpasswd -a "$USER" systemd-network || true
@@ -120,114 +119,14 @@ setup_sudo() {
     echo "$USER ALL=(ALL) NOPASSWD:ALL" | sudo tee -a /etc/sudoers.d/"$USER" >/dev/null
 }
 
-install_base() {
-    # TODO(jwilkins): Make this arch agnostic
-    sudo apt update -qq || true
-    sudo apt -yqq upgrade
-    xargs <packages sudo apt install -yqq --no-install-recommends
-
-    if [[ ! "$IS_SERVER" ]]; then
-        sudo apt -yqq install signal-desktop compton
-    fi
-
-    sudo apt -y autoremove
-    sudo apt autoclean
-    sudo apt clean
-
-    # install polybar
-    if [[ ! -f /usr/local/bin/polybar ]] && [[ ! "$IS_SERVER" ]]; then
-        tmpdir=$(mktemp -d)
-        (
-            cd "$tmpdir" || exit 1
-            git clone https://github.com/jaagr/polybar.git
-            cd polybar
-            ./build.sh
-        ) || fail "couldn't cd to $tmpdir to install polybar"
-    fi
-
-    # install fonts
-    if [[ ! "$IS_SERVER" ]]; then
-        install_fonts
-    fi
+install_programs() {
+    info "installing programs via brew"
+    brew bundle --file=./Brewfile
 }
 
-install_sources() {
-	# set up sources
-	sudo bash -c 'cat <<-EOF > /etc/apt/sources.list.d/google-cloud-sdk.list
-deb http://packages.cloud.google.com/apt cloud-sdk-$(lsb_release -c -s) main
-	EOF'
-	sudo bash -c 'cat <<-EOF > /etc/apt/sources.list.d/google-chrome.list
-deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main
-	EOF'
-	sudo bash -c 'cat <<-EOF > /etc/apt/sources.list.d/signal.list
-deb [arch=amd64] https://updates.signal.org/desktop/apt xenial main
-	EOF'
-	sudo bash -c 'cat <<-EOF > /etc/apt/sources.list.d/signal.list
-deb https://deb.nodesource.com/node_11.x stretch main
-	EOF'
-	sudo bash -c 'cat <<-EOF > /etc/apt/sources.list.d/spotify.list
-deb http://repository.spotify.com stable non-free
-	EOF'
+shell_setup() {
+    info "setting up shell"
 
-    # keys
-    curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
-    curl -s https://dl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
-    curl -s https://updates.signal.org/desktop/apt/keys.asc | sudo apt-key add -
-    sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 0DF731E45CE24F27EEEB1450EFDC8610341D9410 1655A0AB68576280 A87FF9DF48BF1C90
-
-    # ppa
-    sudo add-apt-repository -y ppa:hluk/copyq
-
-    # speed up apt
-    sudo mkdir -p /etc/apt/apt.conf.d
-    sudo rm -f /etc/apt/apt.conf.d/99translations
-    echo 'Acquire::Languages "none";' | sudo tee -a /etc/apt/apt.conf.d/99translations >/dev/null
-}
-
-install_fonts() {
-    mkdir -p "$HOME/.fonts"
-    if fc-list | grep 'mononoki'; then
-        info "mononoki font exists"
-    else
-        tmpdir=$(mktemp -d)
-        info "installing Mononoki font"
-        (
-            cd "$tmpdir" || exit 1
-            url="$(curl -fsSL https://api.github.com/repos/madmalik/mononoki/releases | jq '.[0].assets[0].browser_download_url')"
-            url=$(echo "$url" | tr -d '"')
-            curl -fsSL "$url" -o mononoki.zip
-            if [[ ! -f mononoki.zip ]]; then
-                exit 1
-            fi
-            unzip "mononoki.zip"
-            rm "mononoki.zip"
-            mv ./* "$HOME/.fonts"
-            fc-cache -fv
-        ) || fail "couldn't download Mononoki"
-    fi
-
-    if fc-list | grep 'iosevka'; then
-        info "iosevka font exists"
-    else
-        tmpdir=$(mktemp -d)
-        info "installing Iosevka font"
-        (
-            cd "$tmpdir"
-            url="$(curl -fsSL https://api.github.com/repos/be5invis/Iosevka/releases | jq '.[0].assets[0].browser_download_url')"
-            url=$(echo "$url" | tr -d '"')
-            curl -fsSL "$url" -o iosevka.zip
-            if [[ ! -f iosevka.zip ]]; then
-                exit 1
-            fi
-            unzip "iosevka.zip"
-            rm "iosevka.zip"
-            mv ./ttf/* "$HOME/.fonts"
-            fc-cache -fv
-        ) || fail "couldn't download Iosevka"
-    fi
-}
-
-setup_zsh() {
     # oh-my-zsh
     if ! [ -s "$HOME/.oh-my-zsh" ]; then
         git clone git://github.com/robbyrussell/oh-my-zsh.git "$HOME/.oh-my-zsh"
@@ -242,139 +141,9 @@ setup_zsh() {
     ln -sf "$HOME/.zsh-prompt" "$THEME_DIR/wilkins-custom.zsh-theme"
 }
 
-install_rust() {
-    if ! [ -s "$HOME"/.cargo/bin/rustc ]; then
-        curl -fsSL "https://sh.rustup.rs" | bash
-    fi
-    success "rust installed, running post-install actions"
-
-    # use nightly rust
-    rustup install nightly
-    rustup default nightly
-    rustup update
-
-    rustup run nightly cargo install shellharden \
-        ripgrep \
-        lsd \
-        bat \
-        miniserve \
-        ffsend \
-        hunter \
-        cargo-update || true
-
-    info "updating rust packages"
-    rustup run nightly cargo install-update -a
-}
-
-install_go() {
-    GO_VERSION=$(curl -fsSL "https://golang.org/VERSION?m=text")
-    INSTALLED_VERSION="none"
-    if [ -s /usr/local/go/bin/go ]; then
-        INSTALLED_VERSION="$(go version | cut -d' ' -f3)"
-    fi
-    GO_SRC=/usr/local/go
-    sudo mkdir -p "$GO_SRC"
-    mkdir -p "$HOME/go"
-    if [[ "$INSTALLED_VERSION" != "$GO_VERSION" ]]; then
-        GO_VERSION=${GO_VERSION#go}
-        info "installing new go version: $GO_VERSION"
-        sudo rm -rf "$GO_SRC"
-
-        local_arch=$ARCH
-        if [[ "$local_arch" == "arm" ]]; then
-            local_arch="armv6l"
-        fi
-        url="https://storage.googleapis.com/golang/go$GO_VERSION.$KERNEL-$local_arch.tar.gz"
-        info "downloading from url $url"
-        curl -fsSL "$url" | sudo tar -v -C /usr/local -xz
-
-        info "updating go packages"
-        GOPATH="$HOME/go" go get -u all
-    fi
-
-    success "go installed, running post-install actions"
-    go get -u honnef.co/go/tools/cmd/staticcheck \
-        github.com/prasmussen/gdrive \
-        github.com/motemen/ghq \
-        github.com/evalexpr/makedl \
-        github.com/davecheney/httpstat
-}
-
-install_python3() {
-    info "installing python3"
-    sudo apt install -yqq python3 \
-        python3-pip \
-        python3-setuptools \
-        python3-dev \
-        --no-install-recommends
-    if ! command -v pip3 >/dev/null 2>&1 || ! [ -s "$HOME"/.local/bin/pip3 ]; then
-        tmpdir=$(mktemp -d)
-        curl -fsSL "https://bootstrap.pypa.io/get-pip.py" -o "/$tmpdir/get-pip.py"
-        python3 "/$tmpdir/get-pip.py" --user
-    fi
-    success "python3 and pip installed, running post-install actions"
-    "$HOME"/.local/bin/pip3 install --user --upgrade pip
-    "$HOME"/.local/bin/pip3 install --quiet --user --upgrade \
-        yapf \
-        pipenv \
-        icdiff \
-        pipreqs \
-        magic-wormhole \
-        httpie \
-        docker-compose \
-        grip
-}
-
-install_extras() {
-    install_rust
-    install_go
-    install_python3
-
-    # fzf
-    if ! [ -s "$HOME/.fzf" ]; then
-        info "installing fzf"
-        git clone --depth 1 https://github.com/junegunn/fzf.git "$HOME/.fzf" &&
-            "$HOME/.fzf/install"
-    else
-        info "updating fzf"
-        (
-            cd "$HOME"/.fzf
-            git pull
-            ./install
-        )
-    fi
-    success "fzf installed"
-
-    # yarn
-    info "installing yarn"
-    curl -fsSL "https://dl.yarnpkg.com/debian/pubkey.gpg" | sudo apt-key add -
-    echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
-    sudo apt update -qq && sudo apt install -yqq yarn --no-install-recommends
-    success "yarn installed"
-
-    # yarn post-install
-    yarn global add diff-so-fancy 2>/dev/null
-
-    # docker
-    if ! command -v docker >/dev/null 2>&1; then
-        curl -fsSL "https://get.docker.com" | bash
-    fi
-    success "docker installed"
-
-    # 1password cli
-    if ! [ -s /usr/local/bin/op ]; then
-        OP=op_"$KERNEL"_"$ARCH"_v0.5.5.zip
-        curl -sSLOf "https://cache.agilebits.com/dist/1P/op/pkg/v0.5.5/$OP"
-        unzip "$OP"
-        sudo mv op /usr/local/bin/op
-        rm op.sig "$OP"
-    fi
-    success "1password cli installed"
-
-    install_bin
-}
-
 setup_git() {
+    info "setting up git"
+
     if ! [ -s "$HOME/.gitconfig" ]; then
         my_sed="sed"
         store="cache"
@@ -402,14 +171,9 @@ setup_git() {
         cp git/gitconfig "$HOME/.gitconfig"
 
         $my_sed -e "s/AUTHOR_NAME/$author/g" -e "s/AUTHOR_EMAIL/$email/g" -e "s/GIT_CREDENTIAL_HELPER/$store/g" -i "$HOME/.gitconfig"
-        if ! [ -z "$key" ]; then
+        if [ -n "$key" ]; then
             $my_sed -e "s/AUTHOR_GPG_KEY/$key/g" -e "s/gpgsign = false/gpgsign = true/g" -i "$HOME/.gitconfig"
         fi
-        if command -v ghq >/dev/null 2>&1; then
-            # Use "|" as a delimiter since $HOME/workspace contains "/"
-            $my_sed -e "s|GHQ_ROOT|$HOME/workspace|g" -i "$HOME/.gitconfig"
-        fi
-
         success "created gitconfig"
     else
         info "skipped gitconfig"
@@ -417,6 +181,8 @@ setup_git() {
 }
 
 setup_dotfiles() {
+    info "linking dotfiles"
+
     local overwrite_all=false backup_all=false skip_all=false
     for src in $(find -H "$DOTFILES_ROOT" -maxdepth 2 -name '*.sym' -not -path '*.git*'); do
         dest="$HOME/.$(basename "${src%.*}")"
@@ -425,6 +191,7 @@ setup_dotfiles() {
 }
 
 setup_systemd() {
+    info "setting up systemd"
     for file in $(find -H config.sym/systemd/system -type f -name '*.service'); do
         dest="/etc/systemd/system/$(basename "$file")"
         if ! [ -L "$dest" ]; then
@@ -434,148 +201,69 @@ setup_systemd() {
     done
 }
 
-setup_vim() {
-    # install latest vim
-    if [[ ! -f /usr/local/bin/vim ]]; then
-        tmpdir=$(mktemp -d)
-        (
-            cd "$tmpdir" || exit 1
-            git clone https://github.com/vim/vim.git
-            cd vim/src || exit 1
-            tag=$(git describe --tags)
-            info "checking out tag $tag"
-            git checkout "$tag" || fail "couldn't check out $tag"
-            ./configure --with-features=huge \
-                --enable-multibyte \
-                --enable-rubyinterp=yes \
-                --enable-pythoninterp=yes \
-                --with-python-config-dir=/usr/lib/python2.7/config \
-                --enable-python3interp=yes \
-                --with-python3-config-dir=/usr/lib/python3.5/config \
-                --enable-perlinterp=yes \
-                --enable-luainterp=yes \
-                --enable-gui=gtk2 \
-                --enable-cscope \
-                --prefix=/usr/local
-            make -j VIMRUNTIMEDIR=/usr/local/share/vim/vim81
-            sudo make install
-        ) || fail "couldn't install vim"
-    fi
-
-
-    # run post-install vim steps
-    vim_post_install
-}
-
 vim_post_install() {
-    info "updating submodules"
+    info "init submodules"
     git submodule init
+    info "updating submodules"
     git submodule update --remote --merge --progress
 
+    info "generating tags and installing coc"
     vim -c "helptags ALL" -c q >/dev/null 2>&1
     vim -c ":call coc#util#install()" -c q >/dev/null 2>&1
 }
 
-pre_install() {
-    sudo apt install -yqq --no-install-recommends curl apt-transport-https
+linux_pre_install() {
+    # TODO(evalexpr): make this arch agnostic
+    info "installing linux pre-requisites"
+
+    # speed up apt
+    sudo mkdir -p /etc/apt/apt.conf.d
+    sudo rm -f /etc/apt/apt.conf.d/99translations
+    echo 'Acquire::Languages "none";' | sudo tee -a /etc/apt/apt.conf.d/99translations >/dev/null
+
+    sudo apt update -qq || true
+    sudo apt -yqq upgrade || true
+    sudo apt -y autoremove || true
+    sudo apt autoclean || true
+    sudo apt clean || true
+
+    sudo apt install -yqq --no-install-recommends curl apt-transport-https || true
+
+    # install linuxbrew
+    if ! type brew &> /dev/null; then
+        sh -c "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install.sh)"
+    fi
 }
 
 setup_home() {
-    # check ssh key for host
-    key_path="$HOME/.ssh/$(hostname)"
-    info "ensuring ssh key at $key_path exists"
-    if [[ ! -f "$key_path.pub" ]]; then
-        info "ssh key does not exist, creating"
-        ssh-keygen -t rsa -b 4096 -f "$key_path"
-    fi
-
-    mkdir -p "$HOME"/{workspace/checkouts,tmp,downloads,documents}
-    mkdir -p "$HOME"/media/{pictures/wallpapers,screenshots,videos,music}
+    mkdir -p "$HOME"/.ssh
+    mkdir -p "$HOME"/{workspace/checkouts,tmp}
     mkdir -p "$HOME"/go/src/github.com/evalexpr/
     ln -sf "$HOME"/go/src/github.com/evalexpr/ "$HOME"/workspace/go
 }
 
 post_install() {
+    info "running post install commands"
+
+    vim_post_install
     setup_home
 
-    if [[ ! "$IS_SERVER" ]]; then
-        sudo update-alternatives --install /usr/bin/x-terminal-emulator x-terminal-emulator "$(command -v alacritty)" 100 || true
-        sudo update-alternatives --set x-terminal-emulator "$(command -v alacritty)"
-    fi
-
-    sudo update-alternatives --install /usr/bin/vi vi /usr/local/bin/vim 100
-    sudo update-alternatives --set vi /usr/local/bin/vim
-
-    sudo update-alternatives --install /usr/bin/editor editor /usr/local/bin/vim 100
-    sudo update-alternatives --set editor /usr/local/bin/vim
+    sudo update-alternatives --install /usr/bin/editor editor /usr/bin/vim 100 || true
+    sudo update-alternatives --set editor /usr/bin/vim || true
 
     # change shell to zsh
     if [[ "$SHELL" != *"zsh"* ]]; then
         info "changing shell to zsh"
         chsh -s "$(command -v zsh)"
     fi
-
-    # temporary removal of go.mod, go.sum
-    rm -f go.mod go.sum
 }
 
-install_bin() {
-    info "installing /usr/local/bin scripts"
-    # speedtest
-    curl -sSL https://raw.githubusercontent.com/sivel/speedtest-cli/master/speedtest.py | sudo tee /usr/local/bin/speedtest >/dev/null
-    sudo chmod +x /usr/local/bin/speedtest
-
-    # rsync_tmbackup
-    curl -sSL https://github.com/laurent22/rsync-time-backup/blob/master/rsync_tmbackup.sh | sudo tee /usr/local/bin/rsync_tmbackup.sh >/dev/null
-    sudo chmod +x /usr/local/bin/rsync_tmbackup.sh
-}
-
-setup_mac() {
-    info "setting up macos"
-    cmd="$1"
-
-    if [[ ! -z $cmd && $cmd == "init" ]]; then
-        setup_sudo
-
-        info "running install.sh in macos dir, first run will take a while"
-        ./macos/install.sh
-        success "finished running mac install"
-
-        info "installing fonts, rust, and go"
-        install_fonts
-        install_rust
-
-        info "installing go packages"
-        go get -u honnef.co/go/tools/cmd/staticcheck \
-            github.com/evalexpr/makedl \
-            github.com/davecheney/httpstat
-
-        info "installing pip stuff"
-        pip3 install --user --upgrade pip
-        pip3 install --quiet --user --upgrade \
-            yapf \
-            pipenv \
-            icdiff \
-            pipreqs \
-            magic-wormhole \
-            httpie \
-            docker-compose \
-            grip
-
-        info "installing diff-so-fancy"
-        yarn global add diff-so-fancy 2>/dev/null
-
-        install_bin
-    fi
-
+install_dots() {
+    info "installing dotfiles"
     setup_git
     setup_dotfiles
-    vim_post_install
-    setup_zsh
-    setup_home
-
-    success "installation complete"
-    exit 0
+    shell_setup
+    post_install
 }
 
 main() {
@@ -584,45 +272,47 @@ main() {
     readonly ARCH="$(determine_arch)"
     info "running for kernel: $KERNEL and arch $ARCH"
 
-    # handle macos
-    [[ "$OSTYPE" == "darwin"* ]] && setup_mac "$cmd"
+    if [[ "$OSTYPE" == "darwin"* ]]; then
+        # If we don't pass a command, we just want to install dotfiles
+        if [[ -z $cmd ]]; then
+            install_dots || fail "could not install dotfiles"
+            finish "installation complete"
+        fi
 
-    info "running pre-install"
-    pre_install
+        if [[ $cmd == "git" ]]; then
+            setup_git
+            finish "git installation complete"
+        fi
 
-    if [[ ! -z $cmd && $cmd == "init" ]]; then
-        info "setting up sudo"
-        setup_sudo
+        if [[ $cmd == "init" ]]; then
+            info "running pre-install"
+            ./macos/install.sh
 
-        info "installing sources"
-        install_sources
-
-        info "installing base"
-        install_base
-
-        info "installing extras"
-        install_extras
+            setup_sudo
+            install_programs
+            install_dots
+        fi
     fi
 
-    info "setting up git"
-    setup_git
+    if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+        info "running pre-install"
+        linux_pre_install
 
-    info "linking dotfiles"
-    setup_dotfiles
+        # If we don't pass a command, we just want to install dotfiles
+        if [[ -z $cmd ]]; then
+            install_dots || fail "could not install dotfiles"
+            setup_systemd
+            finish "installation complete"
+        fi
 
-    info "installing oh-my-zsh"
-    setup_zsh
+        if [[ $cmd == "init" ]]; then
+            setup_sudo
+            install_programs
+            install_dots
+        fi
+    fi
 
-    info "setting up systemd"
-    setup_systemd
-
-    info "setting up vim"
-    setup_vim
-
-    info "running post-install"
-    post_install
-
-    success "installation complete"
+    finish "installation complete"
 }
 
 main "$@"
